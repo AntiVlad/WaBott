7f77cf1a507a548ab069fc5e7a2f18b706747dfe
Vlad <70754771+AntiVlad@users.noreply.github.com>
2023-08-10 20:35:13 +0100

    Delete api.js

--- app/api.js
+++
@@ -1,8 +0,0 @@
-const youtubedl = require('youtube-dl-exec')
-
-youtubedl('https://youtu.be/DsC8jQXRbQE',{
-    format:18,
-    output:"MyVideox.mp4",
-    continue:true,
-    yesOverwrites:true
-})
