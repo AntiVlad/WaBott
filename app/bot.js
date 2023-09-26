0ff6ff165d3b20a7dd4385c6e2d7f6c108020163
AntiVlad <vladnyash@gmail.com>
2023-09-26 20:09:35 +0100

    Update bot.js

--- app/bot.js
+++ app/bot.js
@@ -97,7 +97,7 @@ client.on('message', msg => {
     }else if(msg.body === 'Deez') {
 		msg.reply('Nutz');
         console.log(msg.body)
-    }else if(msg.body.isMe) {
+    }else if(msg.body === '@2348157701961') {
 		msg.reply('Hey');
         console.log(msg.body)
     }
